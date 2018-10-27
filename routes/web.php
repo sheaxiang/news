8aa74e8237a4f4ec6fe302a7a2a3d0cf6b38e63d
sheaxiang <642290531@qq.com>
2018-10-27 11:24:06 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::post('/git/web_hook', function (\Illuminate\Http\Request $request) {
 	$target = '/www/wwwroot/news.webooc.com'; // 生产环境web目录
 
 	$token = 'goodbad12';
+
 	dd($request->all());
 	/*$json = json_decode(file_get_contents('php://input'), true);
 
