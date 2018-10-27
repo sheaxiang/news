cd2ecc4cd70efcdb0d7a29fe19a120bbecdb921d
sheaxiang <642290531@qq.com>
2018-10-27 16:28:16 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
@@ -10,7 +11,8 @@ class IndexController extends Controller
 	public function index(Request $request)
 	{
 		$tags = [['name' => '知乎', 'value' => 'zhihu'], ['name' => 'v2ex', 'value' => 'v2ex'], ['name' => 'cnBeta', 'value' => 'cnbeta'], ['name' => '安全客', 'value' => 'anquanke']];
-		$tag = in_array($request->tag, array_column($tags, 'value')) ? $request->tag : 'zhihu';
+
+		$tag = in_array($request->tag, array_column($tags, 'value')) ? $request->tag : Cache::has('tag') ? Cache::get('tag') : 'zhihu';
 
 		if($tag == 'cnbeta') {
 			$query = DB::table('cnbeta')->orderBy('inputtime','desc');
@@ -19,7 +21,10 @@ class IndexController extends Controller
 		}
 
 		$list = $query->paginate(40);
+
 		$record = DB::table('record')->first();
+
+		Cache::forever('tag', $tag);
 
 		return view('index', compact('tag', 'list', 'tags', 'record'));
     }
