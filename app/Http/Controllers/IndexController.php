a7c12a5c7a3078310e031c0c154f836399c36d02
sheaxiang <642290531@qq.com>
2018-10-27 16:31:36 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
 	public function index(Request $request)
 	{
 		$tags = [['name' => '知乎', 'value' => 'zhihu'], ['name' => 'v2ex', 'value' => 'v2ex'], ['name' => 'cnBeta', 'value' => 'cnbeta'], ['name' => '安全客', 'value' => 'anquanke']];
-
+		dd(Cache::has('tag'));
 		$tag = in_array($request->tag, array_column($tags, 'value')) ? $request->tag : Cache::has('tag') ? Cache::get('tag') : 'zhihu';
 
 		if($tag == 'cnbeta') {
