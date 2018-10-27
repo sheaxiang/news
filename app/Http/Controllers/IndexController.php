9b509dc1e2b8b6613f563001e3c22cddd1ce4597
sheaxiang <642290531@qq.com>
2018-10-27 11:59:03 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
 	{
 		$tags = [['name' => '知乎', 'value' => 'zhihu'], ['name' => 'v2ex', 'value' => 'v2ex']];
 		$tag = in_array($request->tag, array_column($tags, 'value')) ? $request->tag : 'zhihu';
-		dd($tag);
+		dd($request->tag);
 		if($tag == 'zhihu') {
 			$list = DB::table('zhihu')->get();
 		} elseif($tag == 'v2ex') {
